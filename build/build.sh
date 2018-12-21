12d19810865762a18452cceffedea288d5999171
Yijun Yu <y.yu@open.ac.uk>
2018-12-21 16:56:05 -0500

    update

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,4 @@
+wget http://www.antlr3.org/download/antlr-3.5.2-runtime.jar
+mv antlr-3.5.2-runtime.jar $PREFIX/lib/antlr.jar
 cmake -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_LIBDIR=lib srcML-src
 make install
