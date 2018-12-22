85ef58e4f53112c3907b88b90a95911b3ac5fa6c
Yijun Yu <y.yu@open.ac.uk>
2018-12-22 07:47:14 +0000

    update

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 INCLUDE_PATH="${PREFIX}/include"
 LIBRARY_PATH="${PREFIX}/lib"
 cp antlr.jar $PREFIX/lib/antlr.jar
-cmake -G ninja -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR srcML-src
+cmake -G Ninja -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR srcML-src
 ninja
 mv $PREFIX/usr/bin/* $PREFIX/bin/
 mv $PREFIX/usr/lib/* $PREFIX/lib/
